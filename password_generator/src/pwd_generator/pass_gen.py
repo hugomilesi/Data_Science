68713478996b1b0bccb049b85aa93439376cef09
Hugo Guilherme <hugogmilesi@gmail.com>
2022-10-13 13:31:32 -0300

    package creation exercise

---
+++ password_generator/src/pwd_generator/pass_gen.py
@@ -0,0 +1,27 @@
+import random
+
+chars = 'abcdefghijklmn0pqrstuvwxyz'
+upper_chars = chars.upper()
+special_chars ='#@'
+
+
+def createPwd(char_length, include_special = False, include_upper = False):
+    password = []
+    for i in range(char_length):
+        
+        if include_special:
+            password.append(random.choice(special_chars))
+            include_special = False
+        elif include_upper:
+            password.append(random.choice(upper_chars))
+            include_upper = False
+        else:
+            password.append(random.choice(chars))
+            
+    print(f"your password is: {''.join(password)}")
+
+
+
+createPwd(8, include_special = True, include_upper = False)    
+
+  
